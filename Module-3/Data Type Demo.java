2ad4ec3bde1101ece411797644b7b0089ed69466
hemanthk9449 <hemanthkumargundimeda@gmail.com>
2025-05-27 00:35:02 +0530

    Create Data Type Demo.java

---
+++ Module-3/Data Type Demo.java
@@ -0,0 +1,14 @@
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        int age = 25;
+        float height = 5.9f;
+        double salary = 55000.50;
+        char grade = 'A';
+        boolean isEmployed = true;
+        System.out.println("Integer: " + age);
+        System.out.println("Float: " + height);
+        System.out.println("Double: " + salary);
+        System.out.println("Char: " + grade);
+        System.out.println("Boolean: " + isEmployed);
+    }
+}
