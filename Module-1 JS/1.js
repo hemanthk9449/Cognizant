579e1cabd816533deda75ac01b85c89a7e5d142f
hemanthk9449 <hemanthkumargundimeda@gmail.com>
2025-05-28 00:04:15 +0530

    Create 1.js

---
+++ Module-1 JS/1.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>JS Basics & Setup</title>
+</head>
+<body>
+  <h1>Community Portal</h1>
+
+  <script>
+    window.onload = function () {
+      alert("Page is fully loaded");
+    }
+  </script>
+</body>
+</html>
