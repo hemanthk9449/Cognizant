6c941fa0efaeccfb331a84b14d84738c80833f0d
hemanthk9449 <hemanthkumargundimeda@gmail.com>
2025-05-27 16:49:13 +0530

    Create File Writing.java

---
+++ Module-3/File Writing.java
@@ -0,0 +1,22 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileWriteExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter text to write to file: ");
+        String text = scanner.nextLine();
+
+        try {
+            FileWriter writer = new FileWriter("output.txt");
+            writer.write(text);
+            writer.close();
+            System.out.println("Data written to output.txt");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+        }
+
+        
+      }
+}
